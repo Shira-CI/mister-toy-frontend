fbc6321422a43d67d880e0282f4182e9e68821f3
Shira Cohen <shira140@gmail.com>
2023-09-06 13:32:53 +0300

    toy-preview style

--- src/cmps/toy.preview.jsx
+++ src/cmps/toy.preview.jsx
@@ -7,23 +7,25 @@ export function ToyPreview({ toy, onRemoveToy, user }) {
         <article className="toy-preview">
             <Link className="details-a-link" to={`/toy/${toy._id}`}>
                 <h4>{toy.title}</h4>
-                {/* {console.log('from toy preview' ,toy)} */}
-                <img src={toy.image} alt="" />
+                <div className="toy-img-container">
+                    <img src={toy.image} alt="" />
+
+                </div>
 
                 <p>Price: <span>{toy.price}$</span></p>
-                {toy.inStock && <span className="toy-available">Toy Available</span>}
-                {!toy.inStock && <span className="toy-available">Toy Unavailable</span>}
+                {!toy.inStock && <span className="toy-unavailable">Out of stock</span>}
             </Link>
-            {user && user.isAdmin && <section>
 
+            {user && user.isAdmin && <section>
                 <Link to={`/toy/edit/${toy._id}`}>
                     <button>Edit</button>
                 </Link>
                 <button onClick={() => { onRemoveToy(toy._id) }}>Remove Toy</button>
             </section>}
-            {(!user || !user.isAdmin) && <section>
+            
+            {(!user || !user.isAdmin) && (toy.inStock) &&
                 <button className="add-to-cart-btn">Add to cart </button>
-            </section>}
+            }
         </article>
     )
 }
