0a33c4c4946325f2f950a19987a638b474571bad
Shira Cohen <shira140@gmail.com>
2023-09-19 16:02:01 +0300

    stars for rating

--- src/cmps/review.preview.jsx
+++ src/cmps/review.preview.jsx
@@ -1,15 +1,23 @@
-
 export function ReviewPreview({ review }) {
 
     const { fullname, rating, description } = review
+
+    function renderStars(rating) {
+        if (!rating) return
+        let stars = []
+
+        for (var i = 0; i < rating; i++) {
+            stars.push(<span key={i}>⭐</span>)
+        }
+        return stars
+    }
 
     return (
         <article className="review-preview">
             <h4> Full Name:
                 <span> {fullname}</span>
             </h4>
-            <h4> Rating:
-                <span>  {rating} </span>
+            <h4> Rating: {renderStars(rating)}
             </h4>
             <h4> Description:
                 <span>{description}</span>
