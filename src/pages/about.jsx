fa784447a2ed2fb81f1ac5db113b6956c2305739
Shira Cohen <shira140@gmail.com>
2023-09-19 16:37:21 +0300

    about page- all mq

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,26 @@ import { GoogleMap } from "../cmps/google-map";
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1>About us</h1>
+        <p>
+
+
+            Mister-toy is the world's leading company in toys, games, and leisure products for children and infants, both globally and in Israel. Toys are sold in over 1,000 stores worldwide.
+
+            In Israel, the chain has been established since 1995 and operates 31 branches nationwide. Additionally, the chain operates an online sales website and a coupon app.
+
+            Mister-toy places emphasis on safety, quality, and service, offering toys, games, gifts, bicycles, outdoor playground equipment, and swimming pools for children of all ages.
+
+            The extensive product range includes the world's leading toy and game brands, children's favorites, and all that is new and trending in the field!
+
+            Leading Brands: LEGO, Playmobil, Barbie, Super Heroes, Disney, Fisher-Price, LOL, You & Me, Fast Lane, Braun, Toy Planet, and more...
+
+            Mister-toy Israel works for the community throughout the year and supports various charities.
+
+        </p>
+        <div className="about-img-container">
+            <img src="https://images.unsplash.com/photo-1599623560574-39d485900c95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
+        </div>
         {/* <GoogleMap/> */}
     </section>
 }
